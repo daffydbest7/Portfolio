bbbab8ba43507dae4fbb73b4cadbfe6b2db9646b
daffydbest7 <daffydbest@gmail.com>
2023-10-26 16:58:32 +0100

    resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -18,7 +18,7 @@ const SocialLinks =() =>{
                 </>
             ),           
             style: 'rounded-tr-md bg-gradient-to-r from-teal-400 to-teal-700 flex justify-between items-center w-40 h-14 px-3  ml-[-75px] hover:ml-[-10px] hover:rounded-md duration-300 hover:scale-110 ',
-            href: '/DAVIDLAWRENCE.pdf',
+            href: '/DavidLawrence_dev.pdf',
             download: true,
         },
         {
